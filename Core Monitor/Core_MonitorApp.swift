830f3dbabf4d83b89e1c503709d6c07dd1d6fc8b
ventaphobia <nazishamin65@gmail.com>
2026-03-07 19:42:22 +0500

    Initial Commit

---
+++ Core Monitor/Core_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_MonitorApp.swift
+//  Core Monitor
+//
+//  Created by bookme on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Core_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
